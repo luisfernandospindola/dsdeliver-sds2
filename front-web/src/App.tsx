915cdff56339879ce0ebfee8beeca7955ac50e50
luisfernandospindola <spinluisfer@gmail.com>
2021-01-09 02:58:17 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './NavBar';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
